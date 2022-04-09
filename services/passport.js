177f76b649b30d84fb7d2db6c102db0024b49870
jykim111 <jykim1@email.unc.edu>
2022-04-08 20:38:25 -0400

    Created different branch

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const GoogleStrategy  = require('passport-google-oauth20')
+const GoogleStrategy = require('passport-google-oauth20')
 const GitHubStrategy = require('passport-github2')
 const LocalStrategy = require('passport-local')
 
@@ -24,43 +24,43 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        const existingUser = await User.findOne({userId: profile.id})
-        if(existingUser){
+}, async (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    const existingUser = await User.findOne({ userId: profile.id })
+    if (existingUser) {
         // we already have a record with the given profile ID
-            done(null, existingUser)
-        }
-        else{// make a new record of the user id in the UserDatabase
-        const user = await new User( {
+        done(null, existingUser)
+    }
+    else {// make a new record of the user id in the UserDatabase
+        const user = await new User({
             userId: profile.id,
             email: profile._json.email,
             name: profile._json.name,
             firstName: profile._json.given_name,
             lastName: profile._json.family_name
         }).save()
-        done(null, user) 
-        }
-    })
+        done(null, user)
+    }
+})
 );
 
 // GitHub Authetication
-passport.use(new GitHubStrategy ({
+passport.use(new GitHubStrategy({
     clientID: keys.gitHubClientID,
     clientSecret: keys.gitHibClientSecret,
     callbackURL: '/auth/github/callback',
     proxy: true
-    }, async (accessToken, refreshToken, profile, done) => {
-        console.log(profile)
-        const existingUser = await User.findOne({userId: profile.id})
-        if(existingUser){
-            done(null, existingUser)
-        }
-        else{
-            const user = await new User({
-                userId: profile.id
-            }).save()
-            done(null, user)
-        }
+}, async (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    const existingUser = await User.findOne({ userId: profile.id })
+    if (existingUser) {
+        done(null, existingUser)
     }
+    else {
+        const user = await new User({
+            userId: profile.id
+        }).save()
+        done(null, user)
+    }
+}
 ))
