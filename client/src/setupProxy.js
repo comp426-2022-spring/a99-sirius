2f01a44e602755d3963e7e6a51ae0dc2cb3ead01
Guillermo Saez <guisaez@email.unc.edu>
2022-04-21 18:21:39 -0400

    Proxy Fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports = function (app) {
     app.use(
-        ['/user', '/auth/google', '/logout', '/auth/github'],
+        ['/user', '/auth/*', '/logout', '/auth/github'],
         createProxyMiddleware({
             target: 'http://localhost:5555'
         })
