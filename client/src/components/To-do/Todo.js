4c1b36b5689a1cbfe3674e3bf469aa3100e6d495
jykim111 <jykim1@email.unc.edu>
2022-04-23 06:02:52 -0400

    Completed Todo.js for To-do app portion

---
+++ client/src/components/To-do/Todo.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function Todo({ text, todo, todos, setTodos }) {
+    // Events
+    const deleteHandler = () => {
+        setTodos(todos.filter((element) => element.id !== todo.id));
+    };
+
+    const completeHandler = () => {
+        setTodos(todos.map(element => {
+            if (element.id === todo.id) {
+                return {
+                    ...element, completed: !element.completed
+                }
+            }
+            return element;
+        }))
+    };
+
+    return (
+        <div className='todo'>
+            <li className={`todo-item ${todo.completed ? "complted" : ""}`}>
+                {text}
+            </li>
+
+            <button onClick={completeHandler} className="complete-button">
+                <i className='fas fa-check'></i>
+            </button>
+
+            <button onClick={deleteHandler} className="trash-button">
+                <i className='fas fa-trash'></i>
+            </button>
+        </div>
+    )
+}
+
+export default Todo
