8e8debb2cde4da2740c8cd55086f8391245c0e12
Guillermo Saez <guisaez@email.unc.edu>
2022-04-27 16:55:09 -0400

    ADD TASK Integration

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -6,40 +6,85 @@ import { Box } from '@mui/material'
 import { Grid } from '@mui/material'
 import { TextField } from '@mui/material'
 import { Typography } from '@mui/material';
+import { FormGroup } from '@mui/material'
+import { FormControlLabel } from '@mui/material'
+import { Switch } from '@mui/material'
 
 const AddTask = (props) => {
 
-    const[openWindow, setOpenWindow] = useState(true)
+    const login = props.login
+    const {open, openWindow} = props
+    const[reminder, setReminder] = useState(false)
 
-    const toggleOpenWindow = () => {
-        setOpenWindow(!openWindow)
+    function handleClose() {
+        openWindow(false)
     }
 
+    const toggleReminder = () => {
+        setReminder(!reminder)
+    }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget)
+        var info = {
+            login: login,
+            name: data.get("taskName"),
+            description: data.get("taskDescription"),
+            reminder: reminder,
+            completed: false
+        }
+        return props.addTask(info)
+    }
 
     return(
-        <Dialog open={openWindow}>
-            <DialogTitle textAlign="center">Add a new Task</DialogTitle>
+        <Dialog open={open}>
+            <DialogTitle textAlign="center">Add New Task</DialogTitle>
             <DialogContent>
-            <Box component="form">
-            <Grid container width={550}>
-                <Grid item width={550} >
+            <Box component="form" marginTop={1} onSubmit={handleSubmit}>
+            <Grid container rowSpacing={2}>
+                <Grid item xs={7} >
                     <TextField
-                    id="Description"
-                    label="Please add a description to your task:"
-                    multiline
+                    id='TaskName'
+                    label="Task Name:"
+                    name="taskName"
+                    required
                     autoFocus
                     fullWidth
+                    size="small"
                     >
                     </TextField>
                 </Grid>
+                <Grid item xs={5}>
+                    <FormGroup>
+                        <FormControlLabel 
+                            size="small" 
+                            labelPlacement="start" 
+                            control={<Switch/>} 
+                            label="Set Reminder"
+                            onClick={toggleReminder}
+                        />
+                    </FormGroup>
                 </Grid>
-                <Box textAlign='center'>
-                    <Button variant='contained'>
-                        Create Task!
-                         </Button>
-                        </Box>
-                <Button onClick={toggleOpenWindow}>
+                <Grid item xs={12}>
+                    <TextField
+                        id="TaskDescription"
+                        label="Description:"
+                        name="taskDescription"
+                        fullWidth
+                        multiline
+                        size={"small"}
+                    >
+                    </TextField>
+                </Grid>
+                <Grid item xs={12} sx={{display: "flex", justifyContent: "center"}}>
+                    <Button type="submit" variant='contained'>
+                        Create Task
+                    </Button>
+                </Grid>
+            </Grid>
+                <Button size={"small"} variant="outlined" onClick={handleClose}>
                 Close
                 </Button>
                 </Box>
