b49af8efd47e18d51183c392a279b5595a463cc4
Ysaid1 <Ysaid@unc.edu>
2022-04-26 22:01:15 -0400

    Working on popup for adding tasks

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,7 +1,11 @@
-import { Button } from '@mui/material'
+import { Button, DialogContent } from '@mui/material'
 import { Dialog } from '@mui/material'
 import React,{ useState } from 'react'
-
+import { DialogTitle } from '@mui/material'
+import { Box } from '@mui/material'
+import { Grid } from '@mui/material'
+import { TextField } from '@mui/material'
+import { Typography } from '@mui/material';
 
 const AddTask = (props) => {
 
@@ -13,9 +17,26 @@ const AddTask = (props) => {
 
     return(
         <Dialog open={openWindow}>
-            <Button onClick={toggleOpenWindow}>
+            <DialogTitle textAlign="center">Add a new Task</DialogTitle>
+            <DialogContent>
+            <Grid container width={550}>
+                <Grid item width={550} >
+                <Typography variant="h6" component="h5">
+                    Task Name:
+                    </Typography>
+                    <TextField
+                    id="Description"
+                    label="Please add a description to your task:"
+                    multiline
+                    autoFocus
+                    fullWidth
+          ></TextField>
+                </Grid>
+                </Grid>
+                <Button onClick={toggleOpenWindow}>
                 Close
-            </Button>
+                </Button>
+                </DialogContent> 
         </Dialog>
     )
 }
